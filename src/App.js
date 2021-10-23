0c772bfedfa02ec355aaacffd0913eae342fc5df
Safi <nassarsafi@gmail.com>
2021-10-23 16:28:54 -0500

    Development lifecycle with automatic deployment to Firebase.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HelloHelloo
+          
         </a>
       </header>
     </div>
