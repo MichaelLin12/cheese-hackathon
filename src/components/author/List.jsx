a8b1fc4358eddd818ee1ffb1d8cc849b757e5e7d
Safi <nassarsafi@gmail.com>
2021-10-23 23:27:43 -0500

    Move create button to bottom of list pages.

--- src/components/author/List.jsx
+++ src/components/author/List.jsx
@@ -45,12 +45,6 @@ function AuthorList({ data, deleteAction }) {
         deleteAction={deleteModalAction}
         cancelAction={cancelModalAction}
       />
-      <div className="mb-4">
-        <Link to="/author/create" className="btn btn-secondary btn-sm">
-          <CubeIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
-          New Distributer
-        </Link>
-      </div>
       <table className="table w-full max-w-screen-lg">
         <thead>
           <tr>
@@ -98,6 +92,14 @@ function AuthorList({ data, deleteAction }) {
           ))}
         </tbody>
       </table>
+
+      <div className="mb-4" style={{paddingTop: '40px'}}>
+        <Link to="/author/create" className="btn btn-secondary btn-sm">
+          <CubeIcon className="w-5 h-5 mr-2 -ml-1" aria-hidden="true" />
+          New Distributer
+        </Link>
+      </div>
+
     </div>
   )
 }
